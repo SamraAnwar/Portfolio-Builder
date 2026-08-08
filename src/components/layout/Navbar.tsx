9753a2dd387d9aacdb810aaaceae24dd5f72875c
SamraAnwar <anwarahmed0333233@gmail.com>
2026-08-08 20:26:06 +0500

    basic update navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,16 +1,37 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+const navLinks = [
+  { name: "Home", path: "/" },
+  { name: "About", path: "/about" },
+  { name: "Templates", path: "/templates" },
+  { name: "Pricing", path: "/pricing" },
+  
+];
+
 
 function Navbar() {
+ 
   return (
     <nav className="flex items-center justify-between px-8 py-4 border-b">
       <h1 className="text-xl font-bold">Portfolio Builder</h1>
 
-      <div className="flex gap-6">
-        <Link to="/">Home</Link>
-         <Link to="/about">About</Link>
-        <Link to="/templates">Templates</Link>
-        <Link to="/pricing">Pricing</Link>
-      </div>
+     <div className="flex items-center gap-8">
+       {navLinks.map((link) => (
+        <NavLink
+  key={link.path}
+  to={link.path}
+  className={({ isActive }) =>
+   `transition-colors ${
+      isActive
+        ? "font-bold text-blue-600"
+        : "text-neutral-600 hover:text-blue-600"
+    }`
+  }
+>
+  {link.name}
+</NavLink>
+))}
+     </div>
     </nav>
   );
 }
